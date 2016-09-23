257c8d09f986376019dfc2ad38476937851392b8
Ivo Kroon <ivokroon@192.168.2.58>
2016-09-23 22:00:07 +0200

    setup done

--- users/views.py
+++ users/views.py
@@ -3,14 +3,7 @@ from django.http import HttpResponse
 from django.template import Context
 from database.models import *
 
+
 # Create your views here.
 def index(request):
-    user = User.objects.all()
-
-    c = Context({
-        'data': user,
-    })
-
-    # u = User(name="lotte", lastName="de korte", email="ivokroo@gmail.com", member_id=2)
-    # u.save()
-    return render(request, 'login/index.html', c)
+    return render(request, 'login/index.html')
